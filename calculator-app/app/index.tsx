42cd9d32fa7081e96cea8f3a1c2f4eeac4dfc48d
RamsesMeza <ramseswardof@gmail.com>
2026-02-18 08:43:29 -0600

    Add button on press action

--- calculator-app/app/index.tsx
+++ calculator-app/app/index.tsx
@@ -3,9 +3,26 @@ import { CustomBtn } from "@/components/CustomBtn";
 import { CustomText } from "@/components/CustomText";
 import { Row } from "@/components/Row";
 import { colors } from "@/constants/colors";
+import { useState } from "react";
 import { StatusBar, StyleSheet, View } from "react-native";
 
 export default function Index() {
+  const [operation, setOperation] = useState<string>("");
+  const [result, setResult] = useState<string>("");
+
+  const onPressBtn = (text: string) => setOperation((prev) => prev + text);
+  const onReset = () => {
+    setOperation("");
+    setResult("");
+  };
+  const onFinishOperation = () => {
+    console.log("Termine la operación");
+    const result = eval(operation);
+
+    if (!result) setResult("Error");
+
+    setResult(result);
+  };
   return (
     <View style={styles.container}>
       <StatusBar barStyle={"light-content"} />
@@ -22,8 +39,8 @@ export default function Index() {
             alignItems: "flex-end",
           }}
         >
-          <CustomText>10 x 20</CustomText>
-          <CustomText variant={"h2"}>500</CustomText>
+          <CustomText>{operation}</CustomText>
+          <CustomText variant={"h2"}>{result}</CustomText>
         </Column>
 
         <Column
@@ -34,9 +51,10 @@ export default function Index() {
         >
           <Row>
             <CustomBtn
-              text={"1"}
+              text={"C"}
               color={colors.lightGray}
               textColor={colors.darkGray}
+              onPress={onReset}
             />
             <CustomBtn
               text={"+/-"}
@@ -48,30 +66,54 @@ export default function Index() {
               color={colors.lightGray}
               textColor={colors.darkGray}
             />
-            <CustomBtn text={"÷"} color={colors.orange} />
+            <CustomBtn
+              text={"÷"}
+              color={colors.orange}
+              onPress={() => onPressBtn("/")}
+            />
           </Row>
           <Row>
-            <CustomBtn text={"1"} />
-            <CustomBtn text={"2"} />
-            <CustomBtn text={"3"} />
-            <CustomBtn text={"-"} color={colors.orange} />
+            <CustomBtn text={"1"} onPress={() => onPressBtn("1")} />
+            <CustomBtn text={"2"} onPress={() => onPressBtn("2")} />
+            <CustomBtn text={"3"} onPress={() => onPressBtn("3")} />
+            <CustomBtn
+              text={"-"}
+              onPress={() => onPressBtn("-")}
+              color={colors.orange}
+            />
           </Row>
           <Row>
-            <CustomBtn text={"4"} />
-            <CustomBtn text={"5"} />
-            <CustomBtn text={"6"} />
-            <CustomBtn text={"x"} color={colors.orange} />
+            <CustomBtn text={"4"} onPress={() => onPressBtn("4")} />
+            <CustomBtn text={"5"} onPress={() => onPressBtn("5")} />
+            <CustomBtn text={"6"} onPress={() => onPressBtn("6")} />
+            <CustomBtn
+              text={"x"}
+              onPress={() => onPressBtn("*")}
+              color={colors.orange}
+            />
           </Row>
           <Row>
-            <CustomBtn text={"7"} />
-            <CustomBtn text={"8"} />
-            <CustomBtn text={"9"} />
-            <CustomBtn text={"+"} color={colors.orange} />
+            <CustomBtn text={"7"} onPress={() => onPressBtn("7")} />
+            <CustomBtn text={"8"} onPress={() => onPressBtn("8")} />
+            <CustomBtn text={"9"} onPress={() => onPressBtn("9")} />
+            <CustomBtn
+              text={"+"}
+              onPress={() => onPressBtn("+")}
+              color={colors.orange}
+            />
           </Row>
           <Row>
-            <CustomBtn variant="large" text={"0"} />
-            <CustomBtn text={"."} />
-            <CustomBtn text={"="} color={colors.orange} />
+            <CustomBtn
+              variant="large"
+              text={"0"}
+              onPress={() => onPressBtn("0")}
+            />
+            <CustomBtn text={"."} onPress={() => onPressBtn(".")} />
+            <CustomBtn
+              text={"="}
+              onPress={onFinishOperation}
+              color={colors.orange}
+            />
           </Row>
         </Column>
       </Column>
